1470dcf79723a1c16ab6f546dbd3af2b40d18170
Matus Kivader <matuspo@gmail.com>
2023-10-31 14:02:54 +0100

    Firebase database added

--- PhoneContacts/Data/FirebaseContactsManager.swift
+++ PhoneContacts/Data/FirebaseContactsManager.swift
@@ -5,25 +5,70 @@
 //  Created by Matus Kivader on 25/10/2023.
 //
 
-import Foundation
+import FirebaseDatabase
+import UIKit
 
 class FirebaseContactsManager: ContactsManager {
     
+    let database = Database.database().reference()
+    
+    var contacts: [ContactModel] = []
+    var loaded = false
+    
     func load() -> [ContactModel] {
-        return [ContactModel(name: "Firebase", surname: "Test", phone: "Phone")]
+        if !loaded {
+            if let contacts = loadFirebase() {
+                self.contacts = contacts
+            } else {
+                var testContact = ContactModel(name: "Firebase", surname: "Test", phone: "Phone")
+                contacts.append(testContact)
+                
+                for contact in contacts {
+                    let object: [String: String] = [
+                        "name": contact.name,
+                        "surname": contact.surname,
+                        "phoneNumber": contact.phone
+                    ]
+                    
+                    database.child(contact.id.description).setValue(object)
+                }
+            }
+            
+            loaded = true
+        }
+//        [ContactModel(name: "Firebase", surname: "Test", phone: "Phone")]
+        return contacts
     }
     
     func add(_ contact: ContactModel) {
+        contacts.append(contact)
         
+        loaded = false
     }
     
     func edit(_ contact: ContactModel) {
+        if let index = contacts.firstIndex(where: { $0.id == contact.id }) {
+            contacts[index] = contact
+        }
         
+        loaded = false
     }
     
     func delete(_ contact: ContactModel) {
+        if let index = contacts.firstIndex(where: { $0.id == contact.id }) {
+            contacts.remove(at: index)
+        }
         
+        loaded = false
     }
     
+    // MARK: - private methods
     
+    private func loadFirebase() -> [ContactModel]? {
+        return nil
+    }
+    
+    private func saveToFirebase(_ data: [ContactModel]) {
+        
+    }
 }
