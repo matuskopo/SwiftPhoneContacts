59c4282265329cee8c70dbb33f7a1f348e616e35
Matus Kivader <matuspo@gmail.com>
2023-11-13 13:32:05 +0100

    contact list style adjusting

--- PhoneContacts/SwiftUI/ContactListView.swift
+++ PhoneContacts/SwiftUI/ContactListView.swift
@@ -19,8 +19,10 @@ struct ContactListView: View {
                 VStack(alignment: .leading) {
                     Text("\(contact.name) \(contact.surname)")
                     Text(contact.phone)
+                        .font(.system(size: 12))
                 }
             }
+            .padding([.top, .bottom], -3)
         }
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
@@ -34,6 +36,8 @@ struct ContactListView: View {
         .onAppear {
             Resources.sharedInstance.dataManager.load { returnedArray in
                 self.contacts = returnedArray
+                
+                self.contacts.sort{$0.name.lowercased() < $1.name.lowercased()}
             }
         }
     }
